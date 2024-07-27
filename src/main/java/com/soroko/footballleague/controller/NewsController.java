c7477236101049c9436668de21aba88f36f1c023
maddojka <yuriy.soroko@gmail.com>
2024-07-28 00:24:06 +0300

    testing file server and add some fixes

--- src/main/java/com/soroko/footballleague/controller/NewsController.java
+++ src/main/java/com/soroko/footballleague/controller/NewsController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class NewsController {
 
     @GetMapping("/news")
-    public String getAllMatches() {
+    public String getNews() {
         return "news";
     }
 }
--- src/main/java/com/soroko/footballleague/controller/StandingsController.java
+++ src/main/java/com/soroko/footballleague/controller/StandingsController.java
@@ -6,15 +6,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/standings")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StandingsController {
     final StandingsService standingsService;
 
+    @GetMapping("/standings")
+    public String getStandings() {
+        return "standings";
+    }
 }
