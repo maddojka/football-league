5e0c8ab4c4844dd05eb4e1945ab7c4f8e77acaea
maddojka <yuriy.soroko@gmail.com>
2024-08-02 23:30:56 +0300

    payment gateway

---
+++ src/main/java/com/soroko/footballleague/controller/PaymentIntentController.java
@@ -0,0 +1,39 @@
+package com.soroko.footballleague.controller;
+
+
+import com.soroko.footballleague.entity.Request;
+import com.soroko.footballleague.entity.Response;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentIntentController {
+    @PostMapping("/create-payment-intent")
+    public Response createPaymentIntent(@RequestBody Request request)
+            throws StripeException {
+        PaymentIntentCreateParams params =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(request.getAmount() * 100L)
+                        .putMetadata("teamName",
+                                request.getTeamName())
+                        .setCurrency("usd")
+                        .setAutomaticPaymentMethods(
+                                PaymentIntentCreateParams
+                                        .AutomaticPaymentMethods
+                                        .builder()
+                                        .setEnabled(true)
+                                        .build()
+                        )
+                        .build();
+
+        PaymentIntent intent =
+                PaymentIntent.create(params);
+
+        return new Response(intent.getId(),
+                intent.getClientSecret());
+    }
+}
